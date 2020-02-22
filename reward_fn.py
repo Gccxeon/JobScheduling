c0e07ed300b1d6088407b12f0a9f341dfdebc1d8
gccxeon <gccxeon@gmail.com>
2020-02-22 23:43:28 +0800

    sync

--- reward_fn.py
+++ reward_fn.py
@@ -1,7 +1,7 @@
 import math
 def scaled_reward(wait_times, wait_time, exec_time, finish_time):
   difference_scale = max(wait_times) - min(wait_times) + 0.01
-  reward = -(wait_time / difference_scale)*20 + 1 / (exec_time + 0.01)
+  reward = -(wait_time / difference_scale)*5 - math.log(difference_scale, 5) + 1 / (exec_time + 0.01)
   return reward
 
 
